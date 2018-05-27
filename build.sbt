23513df819630603ca8e620bfa4e983ce6ee1bae
Dima Statz <dima.statz@cloudlock.com>
2018-05-27 14:34:44 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "simple-flat-json"
+version := "0.1"
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.5.0"
---
+++ src/main/scala/com/dima/JsonUtils.scala
@@ -0,0 +1,18 @@
+package com.dima
+
+object JsonUtils {
+  def flatten(json: Map[String, Any], prefix: String = ""): Map[String, Any] = {
+    val flatMap = json.map(i => i._2 match {
+      case x: Map[String, Any] => x.map(j => flatten(x, s"$prefix${i._1}_and_"))
+      case x: List[Any] => x.zipWithIndex.map(j => Map(s"${prefix}_${i._1}_${j._2}" -> j._1))
+      case _ => List(Map(prefix + i -> i._2))
+    })
+
+    flatMap.flatten.flatMap(i => i.toList).groupBy(i => i._1)
+  }
+
+  def unescape(message: String): String = {
+    val unescaped = StringContext.treatEscapes(message)
+    if(unescaped(0) == '"') unescaped.substring(1, unescaped.length - 2) else unescaped
+  }
+}
