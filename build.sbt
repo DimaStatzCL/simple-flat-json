3474c4ffb827ce77d90fa9837b2f8efa047a2fc7
Dima Statz <dima.statz@cloudlock.com>
2018-05-27 14:58:01 +0300

    add json serialize

--- build.sbt
+++ build.sbt
@@ -3,3 +3,4 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.5.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test
--- src/main/scala/com/dima/JsonUtils.scala
+++ src/main/scala/com/dima/JsonUtils.scala
@@ -1,6 +1,13 @@
 package com.dima
 
+import org.json4s.jackson.JsonMethods.parse
+
 object JsonUtils {
+  
+  def jsonToMap(json: String): Map[String, Any] = {
+    parse(json).values.asInstanceOf[Map[String, Any]]
+  }
+
   def flatten(json: Map[String, Any], prefix: String = ""): Map[String, Any] = {
     val flatMap = json.map(i => i._2 match {
       case x: Map[String, Any] => x.map(j => flatten(x, s"$prefix${i._1}_and_"))
