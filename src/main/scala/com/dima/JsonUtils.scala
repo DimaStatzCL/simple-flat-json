6504022878436d50513e5dc4cc59e75f2c1a4ed4
Dima Statz <dima.statz@cloudlock.com>
2018-05-27 15:32:00 +0300

    fix prefix bugs

--- src/main/scala/com/dima/JsonUtils.scala
+++ src/main/scala/com/dima/JsonUtils.scala
@@ -8,14 +8,20 @@ object JsonUtils {
     parse(json).values.asInstanceOf[Map[String, Any]]
   }
 
+
+  def addPrefix(prefix: String, value: String): String = prefix match {
+    case "" => value
+    case _ => s"${prefix}_and_$value"
+  }
+
   def flatten(json: Map[String, Any], prefix: String = ""): Map[String, Any] = {
     val flatMap = json.map(i => i._2 match {
-      case x: Map[String, Any] => x.map(j => flatten(x, s"$prefix${i._1}_and_"))
-      case x: List[Any] => x.zipWithIndex.map(j => Map(s"${prefix}_${i._1}_${j._2}" -> j._1))
-      case _ => List(Map(prefix -> i._2))
+      case x: Map[String, Any] => x.map(j => flatten(x, s"${addPrefix(prefix, i._1)}"))
+      case x: List[Any] => x.zipWithIndex.map(j => Map(s"${addPrefix(prefix, i._1)}_${j._2}" -> j._1))
+      case _ => List(Map(addPrefix(prefix, i._1) -> i._2))
     })
 
-    flatMap.flatten.flatMap(i => i.toList).groupBy(i => i._1)
+    flatMap.flatten.reduce(_ ++ _)
   }
 
   def unescape(message: String): String = {
--- src/test/scala/com/dima/TestJsonUtils.scala
+++ src/test/scala/com/dima/TestJsonUtils.scala
@@ -6,11 +6,11 @@ class TestJsonUtils extends Matchers with WordSpecLike {
   "Json utils logic" must {
     "flatten json" in {
       val jsonMap = Map(
-        "field_1" -> 5,
-        "field_2" -> List(1,2),
-        "field_3" -> Map(
-          "nested_1" -> 10,
-          "nested_2" -> List(11, 12))
+        "fieldA" -> 5,
+        "fieldB" -> List(1,2),
+        "fieldC" -> Map(
+          "nested_c_a" -> 10,
+          "nested_c_b" -> List(11, 12))
       )
 
       val map = JsonUtils.flatten(jsonMap)
